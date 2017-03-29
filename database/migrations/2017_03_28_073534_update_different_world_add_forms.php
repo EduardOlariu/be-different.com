2277062aa1e1c823dd532a618f4976bbe21059e0
= <=>
2017-03-29 10:28:46 +0300

    Added Business and World profiles

--- database/migrations/2017_03_28_073534_update_different_world_add_forms.php
+++ database/migrations/2017_03_28_073534_update_different_world_add_forms.php
@@ -14,13 +14,14 @@ class UpdateDifferentWorldAddForms extends Migration
     public function up()
     {
         Schema::table('different_world', function (Blueprint $table) {
+            $table->string('name')->nullable();
             $table->string('email')->nullable();
             $table->text('description')->nullable();
             $table->string('address')->nullable();
             $table->string('city')->nullable();
             $table->string('state')->nullable();
             $table->string('zip')->nullable();
-            $table->string('adress')->nullable();
+            $table->string('phone')->nullable();
             $table->string('web')->nullable();
             $table->text('how_different')->nullable();
 
@@ -36,13 +37,14 @@ class UpdateDifferentWorldAddForms extends Migration
     public function down()
     {
         Schema::table('different_world', function (Blueprint $table) {
+            $table->dropColumn('name');
             $table->dropColumn('email');
             $table->dropColumn('description');
             $table->dropColumn('address');
             $table->dropColumn('city');
             $table->dropColumn('state');
             $table->dropColumn('zip');
-            $table->dropColumn('adress');
+            $table->dropColumn('phone');
             $table->dropColumn('web');
             $table->dropColumn('how_different');
 
