68887d7c47f37bc836f379fe9697519451698f36
Eduard Olariu <eduard@aiteh.eu>
2017-05-10 18:09:20 +0300

    finished

--- resources/views/events/show.blade.php
+++ resources/views/events/show.blade.php
@@ -16,51 +16,54 @@
 
 				<div class="panel-body" >
 					<h1 >{{$event->name}}</h1 >
-					<p class="blog-post-meta">
+					<p class="blog-post-meta" >
 						{{$event->user->name}} on {{$event->created_at->toFormattedDateString()}}
-					</p>
+					</p >
 					{{$event->body}}
 					<hr >
-					<div class="card">
-					<div class="comments card-block" >
-						<h2 >Comments</h2 >
-						@if (count($event->comments))
-							<ul class="list-group" >
-								@foreach($event->comments as $comment)
-									<li class="list-group-item" >
-										<strong >
-											{{$comment->user->name}} - &nbsp; {{$comment->created_at->diffForHumans()}}: &nbsp;
-										</strong >
-										{{$comment->body}}
-									</li >
-								@endforeach
-							</ul >
-						@else
-							No comment...
-						@endif
-						<hr >
-						<div class="card " >
-							<div class="card-block" >
+					<div class="card" >
+						<div class="comments card-block" >
+							<h2 >Comments</h2 >
+							@if (count($event->comments))
+								<ul class="list-group" >
+									@foreach($event->comments as $comment)
+										<li class="list-group-item" >
+											<strong >
+												{{$comment->user->name}} -
+												                         &nbsp; {{$comment->created_at->diffForHumans()}}
+												                         : &nbsp;
+											</strong >
+											{{$comment->body}}
+										</li >
+									@endforeach
+								</ul >
+							@else
+								No comment...
+							@endif
+							<hr >
+							<div class="card " >
+								<div class="card-block" >
 								@include('layouts.partials.errors')
-							{!! Form::open(['url'=>"/user/events/$event->id/comments",'method'=>'POST']) !!}
+								{!! Form::open(['url'=>"/user/events/$event->id/comments",'method'=>'POST']) !!}
 
 
-							<!-- Body Form Input -->
-								<div class="form-group" >
-									<input class="form-control" name="body" type="text" placeholder="Your comment here"
-									       id="body" required>
+								<!-- Body Form Input -->
+									<div class="form-group" >
+										<input class="form-control" name="body" type="text"
+										       placeholder="Your comment here"
+										       id="body" required >
+									</div >
+
+									<!-- submit Form Input-->
+									<div class="form-group" >
+										{!! Form::submit('Add Comment',['class' => 'btn btn-primary']) !!}
+									</div >
+
+
+									{!! Form::close() !!}
 								</div >
-								
-								<!-- submit Form Input-->
-								<div class="form-group" >
-									{!! Form::submit('Add Comment',['class' => 'btn btn-primary']) !!}
-								</div >
-
-
-								{!! Form::close() !!}
 							</div >
 						</div >
-					</div>
 					</div >
 				</div >
 			</div >
